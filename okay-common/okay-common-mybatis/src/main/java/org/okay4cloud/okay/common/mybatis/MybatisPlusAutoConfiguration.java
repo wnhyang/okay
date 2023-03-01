084f4978c8c8ca3ac8d9fc92c15449bfc10cfbbc
wnhyang <wnhyang@qq.com>
2023-03-01 08:40:08 +0800

    log4j2引入

--- okay-common/okay-common-mybatis/src/main/java/org/okay4cloud/okay/common/mybatis/MybatisPlusAutoConfiguration.java
+++ okay-common/okay-common-mybatis/src/main/java/org/okay4cloud/okay/common/mybatis/MybatisPlusAutoConfiguration.java
@@ -4,8 +4,13 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import org.okay4cloud.okay.common.mybatis.handler.MybatisPlusMetaObjectHandler;
 import org.okay4cloud.okay.common.mybatis.interceptor.OkayPaginationInnerInterceptor;
+import org.okay4cloud.okay.common.mybatis.resolver.SqlFilterArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * @author wnhyang
@@ -14,7 +19,17 @@ import org.springframework.context.annotation.Configuration;
  * mybatis-plus 统一配置
  **/
 @Configuration(proxyBeanMethods = false)
-public class MybatisPlusAutoConfiguration {
+public class MybatisPlusAutoConfiguration implements WebMvcConfigurer {
+
+
+    /**
+     * SQL 过滤器避免SQL 注入
+     * @param argumentResolvers
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SqlFilterArgumentResolver());
+    }
 
     /**
      * 分页插件, 对于单一数据库类型来说,都建议配置该值,避免每次分页都去抓取数据库类型
