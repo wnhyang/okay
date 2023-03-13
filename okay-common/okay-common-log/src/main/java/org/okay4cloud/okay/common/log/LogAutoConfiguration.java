577f4085330d9f4c339e402f0b16508d93b68e0a
wnhyang <wnhyang@qq.com>
2023-03-13 09:02:12 +0800

    日志切面完善，nacos地址改为host方式配置

--- okay-common/okay-common-log/src/main/java/org/okay4cloud/okay/common/log/LogAutoConfiguration.java
+++ okay-common/okay-common-log/src/main/java/org/okay4cloud/okay/common/log/LogAutoConfiguration.java
@@ -1,8 +1,32 @@
 package org.okay4cloud.okay.common.log;
 
+import lombok.RequiredArgsConstructor;
+import org.okay4cloud.okay.admin.api.feign.RemoteLogService;
+import org.okay4cloud.okay.common.log.aspect.SysLogAspect;
+import org.okay4cloud.okay.common.log.event.SysLogListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 /**
  * @author wnhyang
  * @date 2023/3/8
  **/
+@EnableAsync
+@RequiredArgsConstructor
+@ConditionalOnWebApplication
+@Configuration(proxyBeanMethods = false)
 public class LogAutoConfiguration {
+
+    @Bean
+    public SysLogListener sysLogListener(RemoteLogService remoteLogService) {
+        return new SysLogListener(remoteLogService);
+    }
+
+    @Bean
+    public SysLogAspect sysLogAspect() {
+        return new SysLogAspect();
+    }
+
 }
--- okay-common/okay-common-log/src/main/java/org/okay4cloud/okay/common/log/enums/LogTypeEnum.java
+++ okay-common/okay-common-log/src/main/java/org/okay4cloud/okay/common/log/enums/LogTypeEnum.java
@@ -25,7 +25,6 @@ public enum LogTypeEnum {
      */
     ERROR("9", "错误日志");
 
-
     /**
      * 类型
      */
